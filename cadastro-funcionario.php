87f36d7c21716c9eb81a02fbed5604f72f1b6ff7
Tony Tec <tonytec@hotmail.com.br>
2024-10-08 10:56:54 -0300

    Update cadastro-funcionario.php

--- cadastro-funcionario.php
+++ cadastro-funcionario.php
@@ -2,7 +2,7 @@
 include(__DIR__ . '/session_check.php');
 checkSession();
 include(__DIR__ . '/db_connection.php');
-include '/checar_acesso_de_administrador.php';
+include(__DIR__ . '/checar_acesso_de_administrador.php');
 
 $notificationMessage = null;
 $notificationType = null;
