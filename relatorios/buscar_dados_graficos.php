6df9c9ce492db45a5fd37e2ce9ca3723103420a9
Tony Tec <tonytec@hotmail.com.br>
2024-10-20 23:01:55 -0300

    Update buscar_dados_graficos.php

--- relatorios/buscar_dados_graficos.php
+++ relatorios/buscar_dados_graficos.php
@@ -21,7 +21,42 @@ function buscarDados($query, $conn) {
     return ['labels' => $labels, 'data' => $data];
 }
 
-// Ajuste nos nomes das colunas, se necessário
+// Consulta que calcula o faturamento total (pagamentos - devoluções)
+
+// 1. Faturamento por Mês (considerando devoluções)
+$faturamentoMes = buscarDados("
+    SELECT DATE_FORMAT(po.data_pagamento, '%Y-%m') AS label, 
+           SUM(po.total_pagamento) - IFNULL(SUM(d.total_devolucao), 0) AS value
+    FROM pagamento_os po
+    LEFT JOIN devolucao_os d ON po.ordem_de_servico_id = d.ordem_de_servico_id 
+          AND DATE_FORMAT(po.data_pagamento, '%Y-%m') = DATE_FORMAT(d.data_devolucao, '%Y-%m')
+    WHERE po.data_pagamento IS NOT NULL
+    GROUP BY label
+    ORDER BY label ASC", $conn);
+
+// 2. Faturamento por Semana (considerando devoluções)
+$faturamentoSemana = buscarDados("
+    SELECT CONCAT(YEAR(po.data_pagamento), '-', LPAD(WEEK(po.data_pagamento, 1), 2, '0')) AS label, 
+           SUM(po.total_pagamento) - IFNULL(SUM(d.total_devolucao), 0) AS value
+    FROM pagamento_os po
+    LEFT JOIN devolucao_os d ON po.ordem_de_servico_id = d.ordem_de_servico_id 
+          AND WEEK(po.data_pagamento, 1) = WEEK(d.data_devolucao, 1)
+    WHERE po.data_pagamento IS NOT NULL
+    GROUP BY label
+    ORDER BY label ASC", $conn);
+
+// 3. Faturamento por Funcionário (considerando devoluções)
+$faturamentoFuncionario = buscarDados("
+    SELECT os.criado_por AS label, 
+           SUM(po.total_pagamento) - IFNULL(SUM(d.total_devolucao), 0) AS value
+    FROM pagamento_os po
+    INNER JOIN ordens_de_servico os ON po.ordem_de_servico_id = os.id
+    LEFT JOIN devolucao_os d ON po.ordem_de_servico_id = d.ordem_de_servico_id
+    WHERE po.data_pagamento IS NOT NULL
+    GROUP BY label
+    ORDER BY label ASC", $conn);
+
+// 4. OS por Mês (Sem alteração)
 $osMes = buscarDados("
     SELECT DATE_FORMAT(data_criacao, '%Y-%m') AS label, COUNT(*) AS value 
     FROM ordens_de_servico 
@@ -29,6 +64,7 @@ $osMes = buscarDados("
     GROUP BY label 
     ORDER BY label ASC", $conn);
 
+// 5. OS por Semana (Sem alteração)
 $osSemana = buscarDados("
     SELECT CONCAT(YEAR(data_criacao), '-', LPAD(WEEK(data_criacao, 1), 2, '0')) AS label, 
            COUNT(*) AS value 
@@ -37,31 +73,10 @@ $osSemana = buscarDados("
     GROUP BY label 
     ORDER BY label ASC", $conn);
 
-$faturamentoMes = buscarDados("
-    SELECT DATE_FORMAT(data_pagamento, '%Y-%m') AS label, SUM(total_pagamento) AS value 
-    FROM pagamento_os 
-    WHERE data_pagamento IS NOT NULL
-    GROUP BY label 
-    ORDER BY label ASC", $conn);
-
-$faturamentoSemana = buscarDados("
-    SELECT CONCAT(YEAR(data_pagamento), '-', LPAD(WEEK(data_pagamento, 1), 2, '0')) AS label, 
-           SUM(total_pagamento) AS value 
-    FROM pagamento_os 
-    WHERE data_pagamento IS NOT NULL
-    GROUP BY label 
-    ORDER BY label ASC", $conn);
-
+// 6. OS por Funcionário (Sem alteração)
 $osFuncionario = buscarDados("
     SELECT criado_por AS label, COUNT(*) AS value 
     FROM ordens_de_servico 
-    GROUP BY label 
-    ORDER BY label ASC", $conn);
-
-$faturamentoFuncionario = buscarDados("
-    SELECT criado_por AS label, SUM(total_pagamento) AS value 
-    FROM pagamento_os 
-    INNER JOIN ordens_de_servico ON pagamento_os.ordem_de_servico_id = ordens_de_servico.id 
     GROUP BY label 
     ORDER BY label ASC", $conn);
 
