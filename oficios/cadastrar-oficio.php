d7d325cfb663135bd43f903c3d03b31e5addaad3
Tony Tec <88214774+tonytec21@users.noreply.github.com>
2025-02-04 17:41:57 -0300

    Update cadastrar-oficio.php

--- oficios/cadastrar-oficio.php
+++ oficios/cadastrar-oficio.php
@@ -392,13 +392,25 @@ include(__DIR__ . '/../menu.php');
         </div>  
     </div>
  
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>  
-    <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>  
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script type="text/javascript" src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script>  
-    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>  
-    <script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+<!-- jQuery deve vir primeiro -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>  
+
+<!-- Bootstrap depende do jQuery -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>  
+
+<!-- DataTables principal (depende do jQuery) -->
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>  
+
+<!-- Extensão Bootstrap 4 para DataTables (depende do DataTables) -->
+<script type="text/javascript" src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap4.min.js"></script>  
+
+<!-- DataTables Responsive (depende do DataTables) -->
+<script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/dataTables.responsive.min.js"></script>  
+<script type="text/javascript" src="https://cdn.datatables.net/responsive/2.5.0/js/responsive.bootstrap4.min.js"></script>
+
+<!-- SweetAlert2 pode ser carregado em qualquer momento, mas geralmente no final -->
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
 
 
     <script>
