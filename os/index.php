a0d21f4f623f1951b6047f56be6c31fb1e67f77f
Tony Tec <88214774+tonytec21@users.noreply.github.com>
2025-02-06 08:30:43 -0300

    Update index.php

--- os/index.php
+++ os/index.php
@@ -289,7 +289,7 @@ include(__DIR__ . '/db_connection.php');
             <hr>
             <div class="table-responsive">
                 <h5>Resultados da Pesquisa</h5>
-                <table id="tabelaResultados" class="table table-striped table-bordered" style="zoom: 90%">
+                <table id="tabelaResultados" class="table table-striped table-bordered" style="zoom: 85%">
                     <thead>
                         <tr>
                             <th style="width: 7%;">Nº OS</th>
